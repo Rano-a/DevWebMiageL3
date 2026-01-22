9338c914513d38781aa5f0ba490c1bc63e666a2c
Aaron Cabling <aaron.cblg@gmail.com>
2026-01-22 11:58:49 +0100

    Ajout de la navbar avec scroll

--- TP-Portfolio/script.js
+++ TP-Portfolio/script.js
@@ -9,3 +9,18 @@ burger.addEventListener('click', () => {
 closeBtn.addEventListener('click', () => {
     menu.classList.remove('active');
 });
+
+let lastScrollTop = 0;
+const navbar = document.querySelector(".navbar");
+
+window.addEventListener("scroll", () => {
+  const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+  
+  if (scrollTop > lastScrollTop) {
+    navbar.classList.add("navbar-hidden");
+  } else {
+    navbar.classList.remove("navbar-hidden");
+  }
+  
+  lastScrollTop = scrollTop;
+});

c8481c4517e4330cc00cfc239065052042435847
Aaron Cabling <aaron.cblg@gmail.com>
2026-01-27 09:08:51 +0100

    TD1 - Exercice 1 : div qui change de couleur

--- TD1/script.js
+++ TD1/script.js
@@ -1,3 +1,45 @@
-document.getElementById("btn").addEventListener("click", () => {
-  document.getElementById("result").textContent = "JavaScript côté client 🚀";
+const btn1 = document.getElementById("btn");
+console.log(btn1);
+
+const result = document.getElementById("result");
+console.log(result);
+
+const divCouleur = document.querySelector(".changeCouleur");
+console.log(divCouleur);
+
+const colorTab = [
+  "lightred",
+  "lightgreen",
+  "lightcoral",
+  "lightgoldenrodyellow",
+  "lightpink",
+  "lightseagreen",
+  "lightgray",
+  "lightcyan",
+  "lightyellow",
+  "lightsalmon",
+  "lightblue",
+  "lightsteelblue",
+  "lightturquoise",
+  "lightviolet",
+  "lightkhaki",
+  "lightorchid",
+  "lightmagenta",
+  "lightlime",
+  "lightmaroon",
+  "lightnavy",
+  "lightolive",
+  "lightteal",
+  "lightindigo",
+  "lightplum",
+  "lighttan",
+];
+
+btn1.addEventListener("click", () => {
+  result.textContent = "JavaScript côté client 🚀";
+});
+
+divCouleur.addEventListener("click", () => {
+  divCouleur.style.backgroundColor =
+    colorTab[Math.floor(Math.random() * colorTab.length)];
 });

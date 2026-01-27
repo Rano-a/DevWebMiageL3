734a6917f9974ae4a068e2cd5180f7466cde77b8
Aaron Cabling <aaron.cblg@gmail.com>
2026-01-27 09:53:41 +0100

    TD1 - Exercice 4 : Formulaire et submit

--- TD1/script.js
+++ TD1/script.js
@@ -49,19 +49,15 @@ console.log(header);
 const scrollLevel = document.getElementById("scrollLevel");
 console.log(scrollLevel);
 
+const form = document.getElementById("myForm");
+console.log(form);
+
 btn1.addEventListener("click", () => {
   result.textContent = "JavaScript côté client 🚀";
 });
 
 divCouleur.addEventListener("click", () => {
   divCouleur.style.backgroundColor =
-    colorTab[Math.floor(Math.random() * colorTab.length)];
-});
-
-window.addEventListener("scroll", () => {
-  const scrollY = window.scrollY;
-  scrollLevel.textContent = `${scrollY}`;
-  header.style.backgroundColor =
     colorTab[Math.floor(Math.random() * colorTab.length)];
 });
 
@@ -72,4 +68,21 @@ zoneSouris.addEventListener("mousemove", (event) => {
   const pointeur = document.querySelector(".pointeur");
   pointeur.style.left = `${event.clientX - 5}px`; // 5px pour centrer le pointeur
   pointeur.style.top = `${event.clientY - 5}px`;
+});
+
+window.addEventListener("scroll", () => {
+  const scrollY = window.scrollY;
+  scrollLevel.textContent = `${Math.floor(scrollY)}`;
+  header.style.backgroundColor =
+    colorTab[Math.floor(Math.random() * colorTab.length)];
+});
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const formData = new FormData(form);
+  const nom = formData.get("nom");
+  const prenom = formData.get("prenom");
+  const formResult = document.getElementById("formResult");
+
+  formResult.textContent = `Bonjour ${prenom} ${nom} !`;
 });
